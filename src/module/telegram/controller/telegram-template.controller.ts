6c62642e5db2fcc06c34ef6188699fe5bfe78ddf
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2025-03-14 15:54:31 +0700

    feat: tele template

--- src/module/telegram/controller/telegram-template.controller.ts
+++ src/module/telegram/controller/telegram-template.controller.ts
@@ -1,7 +1,19 @@
-import { Elysia } from 'elysia'
-import { DOC_OPTIONS, ROUTER } from '../../../common'
-import { reqMeta } from '../../../config'
-import { authCheck } from '../../user/auth.middleware'
+import { Elysia, t } from 'elysia'
+import {
+	DOC_DETAIL,
+	DOC_OPTIONS,
+	ErrorResDto,
+	IdsDto,
+	PERMISSION,
+	PaginationReqDto,
+	ROUTER,
+	ResWrapper,
+	authErrors,
+} from '../../../common'
+import { castToRes, reqMeta } from '../../../config'
+import { authCheck, permissionCheck } from '../../user/auth.middleware'
+import { telegramTemplateService } from '../service'
+import { PaginateTeleTemplateResDto, UpsertTeleTemplateDto } from '../type'
 
 export const telegramTemplateController = new Elysia({
 	name: 'TelegramTemplateController',
@@ -10,3 +22,54 @@ export const telegramTemplateController = new Elysia({
 })
 	.use(reqMeta)
 	.use(authCheck)
+	.get(
+		'/',
+		async ({ query }) =>
+			castToRes(await telegramTemplateService.paginate(query)),
+		{
+			beforeHandle: permissionCheck(PERMISSION.TELEGRAM_TEMPLATE_VIEW),
+			query: PaginationReqDto,
+			detail: {
+				...DOC_DETAIL.TELE_TEMPLATE_PAGINATE,
+				security: [{ accessToken: [] }],
+			},
+			response: {
+				200: ResWrapper(PaginateTeleTemplateResDto),
+				...authErrors,
+			},
+		},
+	)
+	.post(
+		'/',
+		async ({ body }) => castToRes(await telegramTemplateService.upsert(body)),
+		{
+			body: UpsertTeleTemplateDto,
+			beforeHandle: permissionCheck(PERMISSION.TELEGRAM_TEMPLATE_UPDATE),
+			detail: {
+				...DOC_DETAIL.TELE_TEMPLATE_UPSERT,
+				security: [{ accessToken: [] }],
+			},
+			response: {
+				200: ResWrapper(t.Void()),
+				400: ErrorResDto,
+				...authErrors,
+			},
+		},
+	)
+	.post(
+		ROUTER.TELEGRAM_TEMPLATE.DEL,
+		async ({ body }) => castToRes(await telegramTemplateService.del(body.ids)),
+		{
+			body: IdsDto,
+			beforeHandle: permissionCheck(PERMISSION.TELEGRAM_TEMPLATE_DELETE),
+			detail: {
+				...DOC_DETAIL.TELE_TEMPLATE_DEL,
+				security: [{ accessToken: [] }],
+			},
+			response: {
+				200: ResWrapper(t.Void()),
+				400: ErrorResDto,
+				...authErrors,
+			},
+		},
+	)
