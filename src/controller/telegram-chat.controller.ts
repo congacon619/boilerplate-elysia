add0a485fa731673501f026d46da09503ba319d8
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2025-04-01 22:05:59 +0700

    fix: fix tele

--- src/controller/telegram-chat.controller.ts
+++ src/controller/telegram-chat.controller.ts
@@ -4,16 +4,16 @@ import {
 	DOC_OPTIONS,
 	ErrorResDto,
 	IdsDto,
-	PERMISSION,
+	PREFIX,
 	PaginationReqDto,
 	ROUTER,
 	ResWrapper,
 	authErrors,
-} from '../../../common'
-import { castToRes, reqMeta } from '../../../config'
-import { authCheck, permissionCheck } from '../../user/auth.middleware'
-import { telegramChatService } from '../service'
-import { PaginateTeleChatResDto, UpsertTeleChatDto } from '../type'
+	token12,
+} from '../common'
+import { castToRes, db, reqMeta } from '../config'
+import { authCheck, permissionCheck } from '../service'
+import { PaginateTeleChatResDto, UpsertTeleChatDto } from './dto'
 
 export const telegramChatController = new Elysia({
 	name: 'TelegramChatController',
@@ -24,9 +24,21 @@ export const telegramChatController = new Elysia({
 	.use(authCheck)
 	.get(
 		'/',
-		async ({ query }) => castToRes(await telegramChatService.paginate(query)),
+		async ({ query: { take, skip } }) => {
+			const [docs, count] = await Promise.all([
+				db.telegramChat.findMany({
+					take,
+					skip,
+				}),
+				db.telegramChat.count(),
+			])
+			return castToRes({
+				docs,
+				count,
+			})
+		},
 		{
-			beforeHandle: permissionCheck(PERMISSION.TELEGRAM_CHAT_VIEW),
+			beforeHandle: permissionCheck('TELE_CHAT.VIEW'),
 			query: PaginationReqDto,
 			detail: {
 				...DOC_DETAIL.TELE_CHAT_PAGINATE,
@@ -40,16 +52,33 @@ export const telegramChatController = new Elysia({
 	)
 	.post(
 		'/',
-		async ({ body }) => castToRes(await telegramChatService.upsert(body)),
+		async ({ body }) => {
+			if (body.id) {
+				await db.telegramChat.update({
+					where: { id: body.id },
+					data: body,
+					select: { id: true },
+				})
+			} else {
+				await db.telegramChat.create({
+					data: {
+						...body,
+						id: token12(PREFIX.TELEGRAM_CHAT),
+					},
+					select: { id: true },
+				})
+			}
+			return castToRes(null)
+		},
 		{
 			body: UpsertTeleChatDto,
-			beforeHandle: permissionCheck(PERMISSION.TELEGRAM_CHAT_UPDATE),
+			beforeHandle: permissionCheck('TELE_CHAT.UPDATE'),
 			detail: {
 				...DOC_DETAIL.TELE_CHAT_UPSERT,
 				security: [{ accessToken: [] }],
 			},
 			response: {
-				200: ResWrapper(t.Void()),
+				200: ResWrapper(t.Null()),
 				400: ErrorResDto,
 				...authErrors,
 			},
@@ -57,16 +86,21 @@ export const telegramChatController = new Elysia({
 	)
 	.post(
 		ROUTER.TELEGRAM_CHAT.DEL,
-		async ({ body }) => castToRes(await telegramChatService.del(body.ids)),
+		async ({ body: { ids } }) => {
+			await db.telegramChat.deleteMany({
+				where: { id: { in: ids } },
+			})
+			return castToRes(null)
+		},
 		{
 			body: IdsDto,
-			beforeHandle: permissionCheck(PERMISSION.TELEGRAM_CHAT_DELETE),
+			beforeHandle: permissionCheck('TELE_CHAT.DELETE'),
 			detail: {
 				...DOC_DETAIL.TELE_CHAT_DEL,
 				security: [{ accessToken: [] }],
 			},
 			response: {
-				200: ResWrapper(t.Void()),
+				200: ResWrapper(t.Null()),
 				400: ErrorResDto,
 				...authErrors,
 			},
--- src/controller/telegram-template.controller.ts
+++ src/controller/telegram-template.controller.ts
@@ -1,24 +1,25 @@
 import { Elysia, t } from 'elysia'
+import { InlineKeyboardButton } from 'node-telegram-bot-api'
 import {
 	DOC_DETAIL,
 	DOC_OPTIONS,
 	ErrorResDto,
 	IdsDto,
-	PERMISSION,
+	PREFIX,
 	PaginationReqDto,
 	ROUTER,
 	ResWrapper,
 	authErrors,
-} from '../../../common'
-import { castToRes, reqMeta } from '../../../config'
-import { authCheck, permissionCheck } from '../../user/auth.middleware'
-import { telegramService, telegramTemplateService } from '../service'
+	token12,
+} from '../common'
+import { castToRes, db, reqMeta } from '../config'
+import { authCheck, permissionCheck, telegramService } from '../service'
 import {
 	PaginateTeleTemplateResDto,
 	SendTelegramMessageDto,
 	SendTemplateDto,
 	UpsertTeleTemplateDto,
-} from '../type'
+} from './dto'
 
 export const telegramTemplateController = new Elysia({
 	name: 'TelegramTemplateController',
@@ -29,10 +30,21 @@ export const telegramTemplateController = new Elysia({
 	.use(authCheck)
 	.get(
 		'/',
-		async ({ query }) =>
-			castToRes(await telegramTemplateService.paginate(query)),
+		async ({ query: { take, skip } }) => {
+			const [docs, count] = await Promise.all([
+				db.telegramTemplate.findMany({
+					take,
+					skip,
+				}),
+				db.telegramTemplate.count(),
+			])
+			return castToRes({
+				docs,
+				count,
+			})
+		},
 		{
-			beforeHandle: permissionCheck(PERMISSION.TELEGRAM_TEMPLATE_VIEW),
+			beforeHandle: permissionCheck('TELE_TEMPLATE.VIEW'),
 			query: PaginationReqDto,
 			detail: {
 				...DOC_DETAIL.TELE_TEMPLATE_PAGINATE,
@@ -46,16 +58,47 @@ export const telegramTemplateController = new Elysia({
 	)
 	.post(
 		'/',
-		async ({ body }) => castToRes(await telegramTemplateService.upsert(body)),
+		async ({
+			body: { id, name, description, message, videos, photos, buttons },
+		}) => {
+			if (id) {
+				await db.telegramTemplate.update({
+					where: { id },
+					data: {
+						name,
+						description,
+						message,
+						videos: videos?.length ? videos : [],
+						photos: photos?.length ? photos : [],
+						buttons: buttons?.length ? buttons : [],
+					},
+					select: { id: true },
+				})
+			} else {
+				await db.telegramTemplate.create({
+					data: {
+						name,
+						description,
+						message,
+						videos: videos?.length ? videos : [],
+						photos: photos?.length ? photos : [],
+						buttons: buttons?.length ? buttons : [],
+						id: token12(PREFIX.TELEGRAM_TEMPLATE),
+					},
+					select: { id: true },
+				})
+			}
+			return castToRes(null)
+		},
 		{
 			body: UpsertTeleTemplateDto,
-			beforeHandle: permissionCheck(PERMISSION.TELEGRAM_TEMPLATE_UPDATE),
+			beforeHandle: permissionCheck('TELE_TEMPLATE.UPDATE'),
 			detail: {
 				...DOC_DETAIL.TELE_TEMPLATE_UPSERT,
 				security: [{ accessToken: [] }],
 			},
 			response: {
-				200: ResWrapper(t.Void()),
+				200: ResWrapper(t.Null()),
 				400: ErrorResDto,
 				...authErrors,
 			},
@@ -63,16 +106,21 @@ export const telegramTemplateController = new Elysia({
 	)
 	.post(
 		ROUTER.TELEGRAM_TEMPLATE.DEL,
-		async ({ body }) => castToRes(await telegramTemplateService.del(body.ids)),
+		async ({ body: { ids } }) => {
+			await db.telegramTemplate.deleteMany({
+				where: { id: { in: ids } },
+			})
+			return castToRes(null)
+		},
 		{
 			body: IdsDto,
-			beforeHandle: permissionCheck(PERMISSION.TELEGRAM_TEMPLATE_DELETE),
+			beforeHandle: permissionCheck('TELE_TEMPLATE.DELETE'),
 			detail: {
 				...DOC_DETAIL.TELE_TEMPLATE_DEL,
 				security: [{ accessToken: [] }],
 			},
 			response: {
-				200: ResWrapper(t.Void()),
+				200: ResWrapper(t.Null()),
 				400: ErrorResDto,
 				...authErrors,
 			},
@@ -80,17 +128,39 @@ export const telegramTemplateController = new Elysia({
 	)
 	.post(
 		ROUTER.TELEGRAM_TEMPLATE.SEND,
-		async ({ body }) =>
-			castToRes(await telegramTemplateService.sentTemplate(body)),
+		async ({ body }) => {
+			const [template, chats] = await Promise.all([
+				db.telegramTemplate.findUnique({
+					where: { id: body.telegramTemplateId },
+				}),
+				db.telegramChat.findMany({
+					where: { id: { in: body.telegramChatIds } },
+				}),
+			])
+			if (!template || !chats.length) {
+				return castToRes(null)
+			}
+
+			await telegramService.jobSendMessage({
+				...template,
+				chatIds: chats.map(x => x.chatId),
+				botId: body.telegramBotId,
+				reply_markup: {
+					inline_keyboard:
+						template.buttons as unknown as InlineKeyboardButton[][],
+				},
+			})
+			return castToRes(null)
+		},
 		{
 			body: SendTemplateDto,
-			beforeHandle: permissionCheck(PERMISSION.TELEGRAM_TEMPLATE_SEND),
+			beforeHandle: permissionCheck('TELE_TEMPLATE.SEND'),
 			detail: {
 				...DOC_DETAIL.TELE_TEMPLATE_SEND,
 				security: [{ accessToken: [] }],
 			},
 			response: {
-				200: ResWrapper(t.Void()),
+				200: ResWrapper(t.Null()),
 				400: ErrorResDto,
 				...authErrors,
 			},
@@ -98,16 +168,19 @@ export const telegramTemplateController = new Elysia({
 	)
 	.post(
 		ROUTER.TELEGRAM_TEMPLATE.MANUAL_SEND,
-		async ({ body }) => castToRes(await telegramService.jobSendMessage(body)),
+		async ({ body }) => {
+			await telegramService.jobSendMessage(body)
+			return castToRes(null)
+		},
 		{
 			body: SendTelegramMessageDto,
-			beforeHandle: permissionCheck(PERMISSION.TELEGRAM_TEMPLATE_SEND),
+			beforeHandle: permissionCheck('TELE_TEMPLATE.SEND'),
 			detail: {
 				...DOC_DETAIL.TELE_SEND_MANUAL,
 				security: [{ accessToken: [] }],
 			},
 			response: {
-				200: ResWrapper(t.Void()),
+				200: ResWrapper(t.Null()),
 				400: ErrorResDto,
 				...authErrors,
 			},
